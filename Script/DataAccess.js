149f0d19f55cb70b3cff9375089eb3059c989dd9
Kevin Knowles <kmknowles001@yahoo.co.uk>
2021-05-29 15:57:38 +0100

    first commit2

--- Script/DataAccess.js
+++ Script/DataAccess.js
@@ -3,27 +3,44 @@
 // console.log("called function.")
 // }
 
-function getServiceMeta(){
+function getServiceMeta()
+{
     
     console.log("getServiceMeta called.")
 
     const Http = new XMLHttpRequest();
     const url='http://localhost:8080/indices/api/meta';
+    
     Http.open("GET", url);
     Http.send();
 
-    Http.onreadystatechange = (e) => {
-
-        var rtn = Http.responseText;
-        var idx =  JSON.parse('{"serviceName": "Index Data As A Service","serviceVersion":"0.0.1"}');
-        var svcMeta = JSON.parse(rtn);
-        //console.log(JSON.parse(Http.responseText));
+    fetch(url).then(function(response){
+        return response.json();
+    })
+    .then(function(myJson){
         
-        // document.getElementById("indexServiceDesc").innerHTML = result.serviceDescription;
-
-        console.log(rtn);
-      }
+        document.getElementById("indexServiceDesc").innerHTML = myJson.serviceName;
+    });
 }
+
+    // Http.open("GET", url);
+    // Http.send();
+
+    // Http.onreadystatechange = (e) => {
+
+    //     var rtn = Http.responseText;
+    //     // var idx =  JSON.parse('{"serviceName": "Index Data As A Service","serviceVersion":"0.0.1"}');
+    //     console.log("hardcoded json");
+    //     var idx =  JSON.parse('{"serviceName":"Index Data As A Service","serviceVersion":"0.0.1","servicePrimarySupportContact":null,"serviceDescription":null}');
+    //     console.log("rest call json");
+    //     var svcMeta = JSON.parse(trim(rtn));
+    //     //console.log(JSON.parse(Http.responseText));
+        
+    //     // document.getElementById("indexServiceDesc").innerHTML = result.serviceDescription;
+
+    //     console.log(rtn);
+      
+
 
 function getHelloWorld(){
     console.log("Hello World has been called.")
